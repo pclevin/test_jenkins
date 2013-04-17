d161000fe9f8e34766e3b918386ab369ecee86f0
levin <pclevinjava@gmail.com>
2013-04-17 11:32:52 +0800

    create a basic project

---
+++ src/main/java/java/com/springmvc/demo/controller/MyMainController.java
@@ -0,0 +1,21 @@
+package com.springmvc.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/myMain")
+public class MyMainController {
+ 
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String showWelcome(ModelMap model) {
+ 
+		model.addAttribute("message", "Hi , Spring 3 MVC Hello World");
+		return "hello";
+ 
+	}
+ 
+}
