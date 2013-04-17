f7265af9c71fe42b02f02f4a9e5370822f4e1c33
levin <pclevinjava@gmail.com>
2013-04-17 11:34:52 +0800

    modify the directory

--- src/main/java/com/springmvc/demo/controller/MyMainController.java
+++ src/main/java/com/springmvc/demo/controller/MyMainController.java
@@ -8,14 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/myMain")
 public class MyMainController {
- 
-	
-	@RequestMapping(method = RequestMethod.GET)
+
+	@RequestMapping(value = "/show", method = RequestMethod.GET)
 	public String showWelcome(ModelMap model) {
- 
 		model.addAttribute("message", "Hi , Spring 3 MVC Hello World");
 		return "hello";
- 
+
 	}
- 
+
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE>
+<html>
+<head><title>我的test</title></head>
+<body bgcolor='white'>
+<a href='<%=request.getContextPath()%>/myMain/show'>我的留言</a><br>
+
+
+
+</body>
+</html>
